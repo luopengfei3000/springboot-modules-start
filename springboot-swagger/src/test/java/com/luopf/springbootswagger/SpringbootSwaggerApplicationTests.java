be32b51280e9dd1be24721b2946a86f04168fb9d
luopengfei3000 <luopengfei3000@163.com>
2020-10-10 18:11:14 +0800

    Swagger学习

---
+++ springboot-swagger/src/test/java/com/luopf/springbootswagger/SpringbootSwaggerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.luopf.springbootswagger;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSwaggerApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
